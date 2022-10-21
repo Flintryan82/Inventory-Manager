687392a4db940234bfa65025f49384e9ea746e82
Mike Allen <freediver72615@gmail.com>
2022-10-20 18:53:32 -1000

    Edit button is now function

--- controllers/edit.js
+++ controllers/edit.js
@@ -7,14 +7,14 @@ module.exports = {
 
     get: function (req, res) {
         let productId = req.params;
-    let iDstring = productId.productId;
-    let idArr = iDstring.split("");
-    idArr.shift();
-    let newId = idArr.join("");
+        let iDstring = productId.productId;
+        let idArr = iDstring.split("");
+        idArr.shift();
+        let itemId = idArr.join("");
 
         // let productId = req.params.productId;
         let loggedIn = req.loggedIn;
-        Product.findById(newId).lean().then(product => {
+        Product.findById(itemId).lean().then(product => {
             let context = {
                 loggedIn,
                 ...product,
@@ -23,7 +23,20 @@ module.exports = {
         });
     },
     post: function (req, res) {
+        let productId = req.params;
+        console.log(productId);
+        let iDstring = productId.productId;
+        let idArr = iDstring.split("");
+        idArr.shift();
+        let itemId = idArr.join("");
+        console.log(itemId);
         let body = req.body;
+        Product.findByIdAndUpdate(itemId, {
+            $set: { ...body }
+        }).then(result=>{
+            console.log(result);
+            res.redirect(`/details/:${itemId}`);
+        });
 
-    }
+    },
 };
